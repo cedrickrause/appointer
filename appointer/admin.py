faa8e01cfd2f5cc6b80c6e10ebff22936dcfa0d5
Cedric Krause <ced.krause@gmail.com>
2024-11-10 17:30:03 +0100

    Tweak admin view for Events

--- appointer/admin.py
+++ appointer/admin.py
@@ -1,6 +1,16 @@
+
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
 from .models import Event, Signup
 
-admin.site.register(Event)
+class EventAdmin(ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["name"]}),
+        ("Date information", {"fields": ["start_timestamp", "end_timestamp"]}),
+    ]
+    list_display = ["name", "start_timestamp", "end_timestamp"]
+    search_fields = ["name"]
+
+admin.site.register(Event, EventAdmin)
 admin.site.register(Signup)
